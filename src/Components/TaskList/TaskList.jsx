7c8b581d781034f0af15998e1e34f091a5db54ae
Siddhika Bande <siddhibande12@gmail.com>
2025-08-08 13:03:46 +0530

    All errors fixed

--- src/Components/TaskList/TaskList.jsx
+++ src/Components/TaskList/TaskList.jsx
@@ -4,31 +4,30 @@ import CompleteTask from './CompleteTask'
 import FailedTask from './FailedTask'
 import NewTask from './NewTask'
 
-const TaskList = ({data}) => {
+const TaskList = ({ data }) => {
   return (
-    <div id='tasklist' className='h-[50%] w-full overflow-x-auto flex items-center justify-start py-5 px-2 mt-10 gap-5'>
+    <div
+      id='tasklist'
+      className='h-[50%] w-full overflow-x-auto flex items-center justify-start py-5 px-2 mt-10 gap-5'
+    >
       {data.tasks.map((elem, index) => {
-
-        if(elem.active){
-          return <AcceptTask  key={index} data={elem}/>
-        }
-        if(elem.completed){
-          return <CompleteTask key={index} data={elem} />
-        }
-        if(elem.failed){
-          return <FailedTask key={index} data={elem} />
-        }
-        if(elem.new){
+        // Check for "new" tasks first so they don't get overridden
+        if (elem.new) {
           return <NewTask key={index} data={elem} />
         }
-        
-
-      }
-      
-    
-    )}
+        if (elem.active) {
+          return <AcceptTask key={index} data={elem} />
+        }
+        if (elem.completed) {
+          return <CompleteTask key={index} data={elem} />
+        }
+        if (elem.failed) {
+          return <FailedTask key={index} data={elem} />
+        }
+        return null // fallback if no condition matches
+      })}
     </div>
   )
 }
 
-export default TaskList 
+export default TaskList
